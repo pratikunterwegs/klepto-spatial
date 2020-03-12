bd3a3d6615824b9fc12817e829a5877998e4450d
pratik <pratikgupte16@gmail.com>
2020-03-12 17:39:35 +0100

    code do moran local quadrant

--- code/code_01_test_import.py
+++ code/code_01_test_import.py
@@ -13,6 +13,7 @@ import os  # has list dir functions etc
 import numpy as np  # some matrix functions
 import imageio
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 # check the current working directory
 os.getcwd()
@@ -48,29 +49,53 @@ def funcImgNames (x):
 
 # test one image
 image = imageio.imread(imgFiles[1])[:,:,1]
-image = image[0:100, 0:100]
+image = image[100:300, 100:300]
 plt.imshow(image)
 
 import pysal.lib
 from pysal.explore.esda.moran import Moran_Local
+from pysal.lib.weights.spatial_lag import lag_spatial
 
-landsize = (100)
-w = pysal.lib.weights.lat2W(landsize/2, landsize/2)
+landsize = (200)
+w = pysal.lib.weights.lat2W(landsize, landsize)
+image = image.reshape(landsize*landsize, 1)
+z = lag_spatial(w, image)
+z = z.reshape(landsize, landsize)
 some_moran = Moran_Local(image, w, permutations=3)
 
 # now reshape
-some_moran = np.array(some_moran.Is).reshape(100, 100)
-x = np.arange(0, 100)
+some_moran = np.array(some_moran.q).reshape(200, 200)
+x = np.arange(0, 200)
 y = x
 x, y = np.meshgrid(x, y)
 
-from mpl_toolkits import mplot3d
-ax = plt.axes(projection='3d')
-ax.plot_surface(x, y, some_moran, rstride=1, cstride=1,
-                cmap='viridis', edgecolor='none')
-ax.set_title('surface')
+plt.imshow(some_moran, cmap=plt.get_cmap('viridis', 4))
+plt.tick_params(bottom=False, left=False, labelbottom=False, labelleft=False)
+cbar = plt.colorbar(ticks=[1,2,3,4], label='LISA class')
+cbar.ax.set_yticklabels(['HH', 'LH', 'LL', 'HL'])
 
-plt.imshow(some_moran, cmap="viridis")
-plt.savefig("figs/fig_example_moran_local.png")
+plt.savefig("figs/fig_example_moran_local_quadrant.png", dpi=300)
+
+####
+# read in agent image
+agents = imageio.imread("data/agents/screenshot00050dummy3.png")
+strat = agents[:,:,1]
+plt.imshow(strat, cmap='viridis')
+plt.tick_params(bottom=False, left=False, labelbottom=False, labelleft=False)
+plt.colorbar()
+plt.clim(250, 256)
+
+agent_size = 200
+w = pysal.lib.weights.lat2W(agent_size, agent_size)
+agent_lisa = Moran_Local(strat, w, permutations=3)
+
+agent_lisa = np.array(agent_lisa.q).reshape(200,200)
+x = np.arange(0,200)
+y = x
+x, y = np.meshgrid(x, y)
+
+plt.imshow(agent_lisa, cmap=plt.get_cmap('inferno_r', 4))
+cbar = plt.colorbar(ticks=[1,2,3,4], label='LISA class')
+cbar.ax.set_yticklabels(['HH', 'LH', 'LL', 'HL'])
 
 # ends here
