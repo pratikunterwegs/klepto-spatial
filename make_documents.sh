ccc38791ef6459a4484c97b05986da3176e95317
pratik <pratikgupte16@gmail.com>
2020-05-09 09:15:21 +0200

    add new docu

--- make_documents.sh
+++ make_documents.sh
@@ -1,1 +1,2 @@
-pandoc Introduction.md Contact.md Description.md Workflow.md Literature.md ../texts/header.yaml --filter=pandoc-citeproc -s -o ../texts/text-project-intro.pdf
+cd klepto_spatial.wiki/
+pandoc Home.md Contact.md Description.md Workflow.md Literature.md ../texts/header.yaml --filter=pandoc-citeproc -s -o ../texts/text-project-intro.pdf
