f5bd33c3b79435c87c008566e10e3a4193a16dfc
pratik <pratikgupte16@gmail.com>
2020-05-11 15:32:26 +0200

    add init to trick python

--- code/__init__.py
+++ code/__init__.py
@@ -1,2 +1,2 @@
 
-from helper_functions import round_any, get_image_names, get_moran_local, get_lisa_proportions
+from helper_functions.py import round_any, get_image_names, get_moran_local, get_lisa_proportions
